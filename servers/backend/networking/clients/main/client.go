0315882f3bdaaf2e8797b557168162abd3f0f595
Chase-Arline <chasearline.jobs@gmail.com>
2019-11-19 20:26:04 -0800

    relative paths + testing on serv

--- servers/backend/networking/clients/main/client.go
+++ servers/backend/networking/clients/main/client.go
@@ -3,6 +3,8 @@ package main
 import (
 	clients "MODE/servers/backend/networking/clients/clientTypes"
 	"fmt"
+	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -18,7 +20,10 @@ func eCheck(err error) {
 }
 
 func rpcStressTest() {
-	cert := "/home/arline/go/src/MODE/servers/backend/certs/ModeCertificate.crt"
+	wd, err := os.Getwd()
+	eCheck(err)
+	fmt.Println(wd)
+	cert := filepath.Join(wd, "../../../", "certs/ModeCertificate.crt")
 	client, err := clients.NewTLSClient("localhost", "3218", cert)
 	eCheck(err)
 	err = client.Connect()
@@ -27,8 +32,15 @@ func rpcStressTest() {
 		time.Sleep(15 * time.Millisecond)
 	}
 	now := time.Now()
+	c := make(chan int)
 	for i := 0; i < 1000; i++ {
-		client.RequestRefreshToken("chasearline", "mypassword")
+		go func(c chan int) {
+			client.RequestRefreshToken("chasearline", "mypassword")
+			c <- 0
+		}(c)
+	}
+	for i := 0; i < 1000; i++ {
+		<-c
 	}
 	fmt.Printf("\n 1000 RPC in %v", time.Since(now))
 }
@@ -36,10 +48,13 @@ func rpcStressTest() {
 func clientStressTest() {
 	numOfClients := 1000
 	c := make(chan int)
+	wd, err := os.Getwd()
+	eCheck(err)
+	fmt.Println(wd)
+	cert := filepath.Join(wd, "../../../", "certs/ModeCertificate.crt")
 	now := time.Now()
 	for i := 0; i < numOfClients; i++ {
 		go func(c chan int, i int) {
-			cert := "/home/arline/go/src/MODE/servers/backend/certs/ModeCertificate.crt"
 			client, err := clients.NewTLSClient("localhost", "3218", cert)
 			eCheck(err)
 			now := time.Now()
@@ -73,7 +88,6 @@ func clientStressTest() {
 				i, connect, refresh, access, test, fail)
 			i = <-c
 		}(c, i)
-		time.Sleep(5 * time.Millisecond)
 	}
 	for i := 0; i < numOfClients; i++ {
 		c <- i
--- servers/backend/networking/servers/main/server.go
+++ servers/backend/networking/servers/main/server.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	servers "MODE/servers/backend/networking/servers/serverTypes"
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 )
 
 func main() {
 	log.Println("Starting main")
-	pub := "/home/arline/go/src/MODE/servers/backend/certs/ModeCertificate.crt"
-
-	priv := "/home/arline/go/src/MODE/servers/backend/certs/ModeKey.key"
-
+	wd, err := os.Getwd()
+	eCheck(err)
+	fmt.Println(wd)
+	pub := filepath.Join(wd, "../../../", "certs/ModeCertificate.crt")
+	priv := filepath.Join(wd, "../../../", "certs/ModeKey.key")
+	log.Println(pub)
 	serv := servers.NewTLSserver("localhost", "3218", pub, priv)
-	serv.Serve()
+	log.Println("serve")
+	err = serv.Serve()
+	log.Fatalf("%v", err)
+}
+
+func eCheck(err error) {
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
 }
