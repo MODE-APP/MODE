45a8b0de303f22ccaca38dfa5b1ecc7b5bc06ef7
Chase-Arline <chasearline.jobs@gmail.com>
2019-11-26 21:47:31 -0800

    starting database structs

---
+++ servers/backend/database/modedb/structdefs.go
@@ -0,0 +1,47 @@
+package modedb
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+//Represents a User in the database
+type User struct {
+	id,
+	profilePicture,
+	backgroundPicture primitive.ObjectID
+
+	email,
+	displayname,
+	username,
+	biography string
+
+	followers []primitive.ObjectID
+	following []primitive.ObjectID
+	posts     []primitive.ObjectID
+}
+
+type Post struct {
+	id,
+	media primitive.ObjectID
+	timeStamp int
+	captionText,
+	captionType string
+	location interface{} //implement later
+	comments []Comment
+}
+
+type Comment struct {
+	id,
+	author primitive.ObjectID
+	text      string
+	timeStamp int
+	likes     []primitive.ObjectID
+	replies   []Comment
+}
+
+type Topic struct {
+	id,
+	featuredImage primitive.ObjectID
+	featured  bool
+	name      string
+	timeStamp int
+	followers []primitive.ObjectID
+}
